9474839d932c0c57c62ebbe4bd927d9e9e13322a
Ivan <ivanshvetcov@gmail.com>
2023-05-18 22:34:12 +0300

    first commit

--- hw8.js
+++ hw8.js
@@ -1,8 +1,8 @@
 function pow(num, degree) {
     let result = num;
     
-    if (degree === 1) {
-        return result;
+    if (degree > 1) {
+        return result * pow(num, degree - 1);
     }
-    return result * pow(num, degree - 1);
+    return result;
 }
